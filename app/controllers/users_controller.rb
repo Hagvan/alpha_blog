371d2703eca63ac95ec4471e00cf0dcf92eb83cd
Hagvan <doronindaniel@gmail.com>
2021-01-14 22:25:16 +0200

    Added session helper method and used them in navigation partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to Alpha Blog #{@user.username}, you have successfully signed up"
       redirect_to root_path
     else
