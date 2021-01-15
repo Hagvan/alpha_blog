2148c23eef1d0768d1538169e43e499bd77d1c7b
hagvan <doronindaniel@gmail.com>
2021-01-15 22:14:23 +0200

    Added admin permissions for users and articles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,14 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?, :require_user
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    begin
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    rescue ActiveRecord::RecordNotFound
+      @current_user = nil
+      session[:user_id] = nil
+      redirect_to root_path
+      return
+    end
   end
 
   def logged_in?
